7bd7332a36b1495b1e46afff1e556e92d365d286
Danilo Spinelli <gunzip@users.noreply.github.com>
2017-11-23 17:36:17 +0100

    chore: added task to sync source control repository code branch (#26)
    * chore: added task to sync source control repository code branch
    
    * fix: code review

---
+++ infrastructure/tasks/15-functions_sync.ts
@@ -0,0 +1,50 @@
+/**
+ * Run this task to sync Azure Functions staging slot
+ * to the source control repository code branch
+ * specified in the configuration file (`tfvars.json`).
+ * 
+ * This is equivalent to push the "Sync" button in the
+ * Azure portal -> Functions -> Deployments blade.
+ * 
+ * This task assumes that the following resources are already created:
+ *  - Resource group
+ *  - Functions (app service)
+ * 
+ */
+// tslint:disable:no-console
+// tslint:disable:no-any
+
+import { login } from "../../lib/login";
+
+import readConfig from "../../lib/config";
+const config = readConfig(__dirname + "/../tfvars.json");
+
+import webSiteManagementClient = require("azure-arm-website");
+
+export const run = async () => {
+  if (!config.functionapp_git_repo) {
+    return Promise.reject(
+      "Deployment from source control repository not configured"
+    );
+  }
+  const loginCreds = await login();
+
+  const webSiteClient = new webSiteManagementClient(
+    loginCreds.creds as any,
+    loginCreds.subscriptionId
+  );
+
+  return webSiteClient.webApps.syncRepositorySlot(
+    config.azurerm_resource_group,
+    config.azurerm_functionapp,
+    config.azurerm_functionapp_slot
+  );
+};
+
+run()
+  .then(r => {
+    if (r) {
+      console.log("Successfully synced functions with source control");
+    }
+  })
+  .catch(console.error);

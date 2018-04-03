bc92d3635ee60159e21e6d6238a4e30d7b312587
Danilo Spinelli <gunzip@users.noreply.github.com>
2018-04-03 14:44:04 +0200

    fix: set client affinity to false (#75)

--- infrastructure/azure.tf
+++ infrastructure/azure.tf
@@ -473,6 +473,7 @@ resource "azurerm_function_app" "azurerm_function_app" {
   resource_group_name       = "${azurerm_resource_group.azurerm_resource_group.name}"
   app_service_plan_id       = "${azurerm_app_service_plan.azurerm_app_service_plan.id}"
   storage_connection_string = "${azurerm_storage_account.azurerm_functionapp_storage_account.primary_connection_string}"
+  client_affinity_enabled   = false
   version                   = "~1"
 
   site_config = {

a8694766470887682f303652dc9aa04623227196
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-05-06 12:02:00 +0200

    [#157287111] added webhook queue

--- infrastructure/azure.tf
+++ infrastructure/azure.tf
@@ -68,6 +68,11 @@ variable "azurerm_storage_queue_emailnotifications" {
   description = "Name of the storage queue for email notifications"
 }
 
+variable "azurerm_storage_queue_webhooknotifications" {
+  type        = "string"
+  description = "Name of the storage queue for notifications sent to webhook"
+}
+
 variable "azurerm_storage_queue_createdmessages" {
   type        = "string"
   description = "Name of the storage queue for created messages"
@@ -420,6 +425,12 @@ resource "azurerm_storage_container" "azurerm_storage_container" {
 
 resource "azurerm_storage_queue" "azurerm_storage_queue_emailnotifications" {
   name                 = "${var.azurerm_storage_queue_emailnotifications}"
+  resource_group_name  = "${azurerm_resource_group.azurerm_resource_group.name}"
+  storage_account_name = "${azurerm_storage_account.azurerm_storage_account.name}"
+}
+
+resource "azurerm_storage_queue" "azurerm_storage_queue_webhooknotifications" {
+  name                 = "${var.azurerm_storage_queue_webhooknotifications}"
   resource_group_name  = "${azurerm_resource_group.azurerm_resource_group.name}"
   storage_account_name = "${azurerm_storage_account.azurerm_storage_account.name}"
 }
